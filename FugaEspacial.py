857e28c1f800a9fb57a13bfed6602ddbc38d04ed
Glória Brito <gloriafbrito@gmail.com>
2023-07-12 01:31:20 -0300

    perform import from pygame

---
+++ FugaEspacial.py
@@ -0,0 +1,11 @@
+"""
+Jogo: Fuga Espacial
+Descrição: Um grupo de diplomatas escapam de uma fortaleza estrelar a bordo de uma nave danificada.
+    A nave precisa se desviar das ameaças e sobreviver até atingir a zona de segurança diplomática.
+"""
+
+import pygame
+
+# Inicia o jogo: Cria o objeto game e chama o loop básico
+game = Game("resolution", "fullscreen")
+game.loop()
